e304dea2ea7e92edccc4acfe309e194c02da361c
Kumari Anushka <anu.sb0340@gmail.com>
2022-02-22 00:40:37 +0530

    added img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import image from "../assets/img.png"
 function Home() {
     return (
         <header>
             <div className="pizza">
-                <img src={"https://cdn-icons.flaticon.com/png/512/2497/premium/2497913.png?token=exp=1645466911~hmac=ec22f0425c164e75e4a075f5676d722d"} alt="" />
+                <img src={image} alt="" />
             </div>
             <div className="headings">
                 <h2>Cheesy Pizzas Here</h2>
